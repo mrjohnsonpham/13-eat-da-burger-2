37f6e477f1ca0c61e7013f377f017eff1a083d9d
Johnson Pham <mrjohnsonpham@gmail.com>
2020-08-29 16:14:53 -0400

    Set up connection and database including seeds

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost", //change host if needed
+        port: 3306, //change port number if needed
+        user: "root", //enter in your mysql username
+        password: "Iloverocky21!", //enter your password
+        database: "burgers_db"
+    });
+}
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ('Juicy-Lucy');
+INSERT INTO burgers (name) VALUES ('Bacon-Cheese');
+INSERT INTO burgers (name) VALUES ('Truffle-Mushroom-Aioli');
+INSERT INTO burgers (name) VALUES ('Sarahtonin-Burger');
+INSERT INTO burgers (name) VALUES ('Queen-Elizabeth-Burger');
+
